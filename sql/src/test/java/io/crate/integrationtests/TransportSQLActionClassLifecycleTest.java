fbbb2c30e32a1dbde979a9ccb1d6e7db18a9f964
Matthias Wahl <matthias@crate.io>
2015-01-27 01:04:31 +0100

    add integration tests for CURRENT_TIMESTAMP

--- sql/src/test/java/io/crate/integrationtests/TransportSQLActionClassLifecycleTest.java
+++ sql/src/test/java/io/crate/integrationtests/TransportSQLActionClassLifecycleTest.java
@@ -646,4 +646,24 @@ public class TransportSQLActionClassLifecycleTest extends ClassLifecycleIntegrat
             assertThat((Boolean) response.rows()[i][3], is(Version.CURRENT.snapshot()));
         }
     }
+
+    @Test
+    public void selectCurrentTimestamp() throws Exception {
+        long before = System.currentTimeMillis();
+        SQLResponse response = executor.exec("select CURRENT_TIMESTAMP from sys.cluster");
+        long after = System.currentTimeMillis();
+
+        assertThat(response.cols(), arrayContaining("CURRENT_TIMESTAMP"));
+        assertThat((Long)response.rows()[0][0], allOf(greaterThanOrEqualTo(before), lessThanOrEqualTo(after)));
+    }
+
+    @Test
+    public void selectWhereEqualCurrentTimestamp() throws Exception {
+        SQLResponse response = executor.exec("select * from sys.cluster where CURRENT_TIMESTAMP = CURRENT_TIMESTAMP");
+        assertThat(response.rowCount(), is(1L));
+
+        response = executor.exec("select * from sys.cluster where CURRENT_TIMESTAMP > CURRENT_TIMESTAMP");
+        assertThat(response.rowCount(), is(0L));
+    }
+
 }
